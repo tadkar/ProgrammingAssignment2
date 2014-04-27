548502ff53c6719af19847433b76fc9ff1667ed4
tadkar <tadkar@yahoo.com>
2014-04-27 09:20:47 +0100

    Update cachematrix.R
    Completed assignment with comments. Included helpful forum post

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is a function that contains the value of a matrix
+## together with four functions that operate on that matrix
+## cacheSolve finds and caches the inverse of the special matrix type 
+## created by makeCacheMatrix
 
-## Write a short comment describing this function
+## The way makeCacheMatrix works is by using the value m as a flag
+## The flag is NULL if the data is fresh 
+## and contains the inverse if the data has not changed 
 
-makeCacheMatrix <- function(x = matrix()) {
+## This link is *very* helpful
+## https://class.coursera.org/rprog-002/forum/thread?thread_id=696
 
+
+makeCacheMatrix <- function(x = numeric()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y             ## This double arrow set <<- sets the value of x in the parent frame
+    inv <<- NULL        ## This double arrow set <<- sets the value of inv in the parent frame
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse ##store the inverse and set the flag
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function first checks if the inverse has already been 
+## found by checking the flag set in the special matrix type
+## If the flag is not NULL, the inverse can be retreived
+## Else the inverse is found and the flag is set to the inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve function(x, ...) {
+  ## Return the inverse of x
+  i <- x$getInverse() 
+  if(!is.null(i)) { ## check to see if the inverse has already been computed
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  matrixInv <- solve(data, ...)
+  x$setInverse(matrixInv) ##store the inverese. This function will set the flag for us
+  matrixInv
 }
